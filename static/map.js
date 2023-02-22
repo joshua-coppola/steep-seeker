3b9e63570e9937749ab06d2ccba9075b3aaa5bdd
joshua-coppola <joshua.coppola@gmail.com>
2023-02-21 20:10:11 -0500

    improved map navigation, ui tweaks

--- static/map.js
+++ static/map.js
@@ -1,1 +1,14 @@
-var instance = panzoom(document.getElementById('map'))
+//var instance = panzoom(document.getElementById('map'))
+
+const elem = document.getElementById('map')
+const parent = elem.parentElement
+const zoom_in = document.getElementById('zoom_in')
+const zoom_out = document.getElementById('zoom_out')
+const zoom_reset = document.getElementById('zoom_reset')
+
+const panzoom = Panzoom(elem, {maxScale: 100, startScale: 1})
+
+zoom_in.addEventListener('click', panzoom.zoomIn)
+zoom_out.addEventListener('click', panzoom.zoomOut)
+zoom_reset.addEventListener('click', panzoom.reset)
+parent.addEventListener('wheel', panzoom.zoomWithWheel)
