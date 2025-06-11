fac9a44fdc55a0a7fcad0b9e1a36277b3d0cd793
joshua-coppola <joshua.coppola99@gmail.com>
2025-06-10 21:32:00 -0400

    added get_trails function, uses shapely geometry

--- test/test_core/test_osm/test_osm_processor.py
+++ test/test_core/test_osm/test_osm_processor.py
@@ -12,3 +12,21 @@ def test_OSMProcessor():
     assert len(osm_processor.trail_relations) == 1
 
     assert len(osm_processor.lifts) == 20
+
+
+def test_get_trails():
+    osm_processor = OSMProcessor("test/test_core/test_osm/test.osm")
+
+    trails = osm_processor.get_trails()
+
+    assert len(trails) == 159
+    assert len(str(trails["w10"]["geometry"])) == 98
+
+
+def test_get_lifts():
+    osm_processor = OSMProcessor("test/test_core/test_osm/test.osm")
+
+    lifts = osm_processor.get_lifts()
+
+    assert len(lifts) == 20
+    assert len(str(lifts["w113"]["geometry"])) == 55
