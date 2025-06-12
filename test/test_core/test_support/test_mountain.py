0f96c28aab726ac1355a69ada7320c8b6fc5c009
joshua-coppola <joshua.coppola99@gmail.com>
2025-06-12 19:02:01 -0400

    added mountain data class

--- test/test_core/test_support/test_mountain.py
+++ test/test_core/test_support/test_mountain.py
@@ -1,8 +1,31 @@
-from core.support.mountain import Trail, Lift
+from core.support.mountain import Mountain, Trail, Lift
+from core.support.states import State, Region
+
 from shapely import LineString
+from datetime import datetime
+import pytest
 
 
-def test_trail():
+@pytest.fixture
+def mountain():
+    mountain_dict = {
+        "id": 1,
+        "name": "Test",
+        "state": State("VT"),
+        "direction": "n",
+        "season_passes": ["Epic", "Ikon"],
+        "trail_count": 42,
+        "lift_count": 0,
+        "vertical": 1024,
+        "difficulty": 89,
+        "beginner_friendliness": 1,
+    }
+
+    return Mountain(**mountain_dict)
+
+
+@pytest.fixture
+def trail():
     trail_dict = {
         "id": "w1000",
         "geometry": LineString([[1, 1], [0, 0]]),
@@ -14,9 +37,59 @@ def test_trail():
         "park": False,
     }
 
-    trail = Trail(**trail_dict)
+    return Trail(**trail_dict)
 
-    assert trail.geometry == trail_dict["geometry"]
+
+@pytest.fixture
+def lift():
+    lift_dict = {
+        "id": "w1001",
+        "geometry": LineString([[1, 1], [0, 0]]),
+        "name": "Test",
+        "lift_type": "chair_lift",
+        "occupancy": 4,
+        "capacity": 1200,
+        "detatchable": False,
+        "bubble": False,
+        "heating": False,
+    }
+
+    return Lift(**lift_dict)
+
+
+def test_mountain(mountain):
+    assert mountain.last_updated.date() == datetime.now().date()
+
+
+def test_mountain_region(mountain):
+    assert mountain.region() == Region.NORTHEAST
+
+
+def test_mountain_bearing(mountain):
+    assert mountain.bearing() == 180
+
+    mountain.direction = "invalid"
+
+    with pytest.raises(Exception) as exc_info:
+        mountain.bearing()
+
+    assert "Invalid direction value:" in exc_info.value.args[0]
+
+
+def test_mountain_from_db():
+    id = "w1000"
+
+    mountain = Mountain.from_db(id)
+
+    assert mountain == "TODO"
+
+
+def test_mountain_to_db(mountain):
+    assert mountain.to_db() == "TODO"
+
+
+def test_trail(trail):
+    assert trail.geometry == LineString([[1, 1], [0, 0]])
 
 
 def test_trail_from_db():
@@ -27,39 +100,12 @@ def test_trail_from_db():
     assert trail == "TODO"
 
 
-def test_trail_to_db():
-    trail_dict = {
-        "id": "w1000",
-        "geometry": LineString([[1, 1], [0, 0]]),
-        "name": "Test",
-        "official_rating": "Expert",
-        "gladed": True,
-        "area": False,
-        "ungroomed": False,
-        "park": False,
-    }
-
-    trail = Trail(**trail_dict)
-
+def test_trail_to_db(trail):
     assert trail.to_db() == "TODO"
 
 
-def test_lift():
-    lift_dict = {
-        "id": "w1001",
-        "geometry": LineString([[1, 1], [0, 0]]),
-        "name": "Test",
-        "lift_type": "chair_lift",
-        "occupancy": 4,
-        "capacity": 1200,
-        "detatchable": False,
-        "bubble": False,
-        "heating": False,
-    }
-
-    lift = Lift(**lift_dict)
-
-    assert lift.geometry == lift_dict["geometry"]
+def test_lift(lift):
+    assert lift.geometry == LineString([[1, 1], [0, 0]])
 
 
 def test_lift_from_db():
@@ -70,19 +116,5 @@ def test_lift_from_db():
     assert lift == "TODO"
 
 
-def test_lift_to_db():
-    lift_dict = {
-        "id": "w1001",
-        "geometry": LineString([[1, 1], [0, 0]]),
-        "name": "Test",
-        "lift_type": "chair_lift",
-        "occupancy": 4,
-        "capacity": 1200,
-        "detatchable": False,
-        "bubble": False,
-        "heating": False,
-    }
-
-    lift = Lift(**lift_dict)
-
+def test_lift_to_db(lift):
     assert lift.to_db() == "TODO"
