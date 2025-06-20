d5a2698e19d611d5edcc76fa2b121b64695ccd3e
joshua-coppola <joshua.coppola99@gmail.com>
2025-06-20 14:49:05 -0400

    split up mountains file, made mountains class more flexible

---
+++ core/support/lift.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass, fields
+from typing import Self, Optional
+
+
+@dataclass
+class Lift:
+    """
+    Lift dataclass that contains all information about a specific lift.
+    An existing lift can be loaded from the DB with from_db, and a new
+    or updated lift can be saved back to the DB with to_db.
+    """
+
+    id: str
+    mountain_id: int
+    geometry: str
+    name: str
+    lift_type: str
+    occupancy: int
+    capacity: int
+    detatchable: bool
+    bubble: bool
+    heating: bool
+    length: Optional[float] = None
+    vertical: Optional[float] = None
+    average_slope: Optional[float] = None
+
+    def from_db(id: str) -> Self:
+        """
+        Gets lift data from database and returns a Lift object
+        """
+        return "TODO"
+
+    def to_db(self) -> None:
+        # check that all fields have been populated before saving
+        missing_fields = [f.name for f in fields(self) if getattr(self, f.name) is None]
+        if len(missing_fields) > 0:
+            raise ValueError(f"The following fields are missing: {missing_fields}")
+        """
+        Updates DB record with the values in the dataclass
+        """
+        return "TODO"
---
+++ test/test_core/test_support/conftest.py
@@ -0,0 +1,60 @@
+import pytest
+from shapely import LineString
+
+from core.support.mountain import Mountain
+from core.support.trail import Trail
+from core.support.lift import Lift
+from core.support.states import State
+
+
+@pytest.fixture
+def mountain(trail, lift):
+    mountain_dict = {
+        "id": 1,
+        "name": "Test",
+        "state": State("VT"),
+        "direction": "n",
+        "season_passes": ["Epic", "Ikon"],
+        "vertical": 1024,
+        "difficulty": 89,
+        "beginner_friendliness": 1,
+        "trails": {"w1000": trail},
+        "lifts": {"w1001": lift},
+    }
+
+    return Mountain(**mountain_dict)
+
+
+@pytest.fixture
+def trail():
+    trail_dict = {
+        "id": "w1000",
+        "mountain_id": 1,
+        "geometry": LineString([[1, 1], [0, 0]]),
+        "name": "Test",
+        "official_rating": "Expert",
+        "gladed": True,
+        "area": False,
+        "ungroomed": False,
+        "park": False,
+    }
+
+    return Trail(**trail_dict)
+
+
+@pytest.fixture
+def lift():
+    lift_dict = {
+        "id": "w1001",
+        "mountain_id": 1,
+        "geometry": LineString([[1, 1], [0, 0]]),
+        "name": "Test",
+        "lift_type": "chair_lift",
+        "occupancy": 4,
+        "capacity": 1200,
+        "detatchable": False,
+        "bubble": False,
+        "heating": False,
+    }
+
+    return Lift(**lift_dict)
