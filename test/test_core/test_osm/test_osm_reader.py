dac971ecbccc23148337ac0859742a307a31a3d1
joshua-coppola <joshua.coppola99@gmail.com>
2025-06-24 19:34:53 -0400

    used fixture for tests instead of hardcoded filepath in each test

--- test/test_core/test_osm/test_osm_reader.py
+++ test/test_core/test_osm/test_osm_reader.py
@@ -1,9 +1,9 @@
 from core.osm.osm_reader import OSMHandler
 
 
-def test_OSMHandler():
+def test_OSMHandler(osm_file):
     osm_handler = OSMHandler()
-    osm_handler.apply_file("test/test_core/test_osm/test.osm")
+    osm_handler.apply_file(osm_file)
 
     assert len(osm_handler.nodes) == 17415
     assert len(osm_handler.ways) == 829
