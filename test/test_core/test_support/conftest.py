8bdf16fdfda7f9621cabf69bdd37be7d6813e9f4
joshua-coppola <joshua.coppola99@gmail.com>
2025-08-19 22:12:01 -0400

    improved db unit test coverage

--- test/test_core/test_support/conftest.py
+++ test/test_core/test_support/conftest.py
@@ -7,27 +7,28 @@ from core.support.trail import Trail
 from core.support.lift import Lift
 from core.datamodels.state import State
 from core.datamodels.season_pass import Season_Pass
+from core.datamodels.database import MountainTable, TrailTable, LiftTable
 
 
 @pytest.fixture
 def mountain(trail, lift):
     mountain_dict = {
-        "mountain_id": 1,
-        "name": "Test",
-        "state": State.VERMONT,
-        "direction": "n",
-        "coordinates": Point(1, 1),
-        "season_passes": [Season_Pass.EPIC, Season_Pass.IKON],
-        "url": "https://test.com",
-        "vertical": 1024,
-        "difficulty": 89,
-        "beginner_friendliness": 1,
-        "average_icy_days": 25,
-        "average_snow": 150,
-        "average_rain": 10,
-        "trails": {"w1000": trail},
-        "lifts": {"w1001": lift},
-        "last_updated": datetime(2000, 2, 5, 12, 30, 5),
+        MountainTable.mountain_id: 1,
+        MountainTable.name: "Test",
+        MountainTable.state: State.VERMONT,
+        MountainTable.direction: "n",
+        MountainTable.coordinates: Point(1, 1),
+        MountainTable.season_passes: [Season_Pass.EPIC, Season_Pass.IKON],
+        MountainTable.url: "https://test.com",
+        MountainTable.vertical: 1024,
+        MountainTable.difficulty: 89,
+        MountainTable.beginner_friendliness: 1,
+        MountainTable.average_icy_days: 25,
+        MountainTable.average_snow: 150,
+        MountainTable.average_rain: 10,
+        "trails": {trail.trail_id: trail},
+        "lifts": {lift.lift_id: lift},
+        MountainTable.last_updated: datetime(2000, 2, 5, 12, 30, 5),
     }
 
     return Mountain(**mountain_dict)
@@ -36,20 +37,20 @@ def mountain(trail, lift):
 @pytest.fixture
 def trail():
     trail_dict = {
-        "trail_id": "w1000",
-        "mountain_id": 1,
-        "geometry": LineString([[1, 1], [0, 0]]),
-        "name": "Test",
-        "official_rating": "Expert",
-        "gladed": True,
-        "area": False,
-        "ungroomed": False,
-        "park": False,
-        "length": 1,
-        "vertical": 1,
-        "difficulty": 1.0,
-        "max_slope": 1.0,
-        "average_slope": 1.0,
+        TrailTable.trail_id: "w1000",
+        TrailTable.mountain_id: 1,
+        TrailTable.geometry: LineString([[1, 1], [0, 0]]),
+        TrailTable.name: "Test",
+        TrailTable.official_rating: "Expert",
+        TrailTable.gladed: True,
+        TrailTable.area: False,
+        TrailTable.ungroomed: False,
+        TrailTable.park: False,
+        TrailTable.length: 1,
+        TrailTable.vertical: 1,
+        TrailTable.difficulty: 1.0,
+        TrailTable.max_slope: 1.0,
+        TrailTable.average_slope: 1.0,
     }
 
     return Trail(**trail_dict)
@@ -58,19 +59,19 @@ def trail():
 @pytest.fixture
 def lift():
     lift_dict = {
-        "lift_id": "w1001",
-        "mountain_id": 1,
-        "geometry": LineString([[1, 1], [0, 0]]),
-        "name": "Test",
-        "lift_type": "chair_lift",
-        "occupancy": 4,
-        "capacity": 1200,
-        "detachable": False,
-        "bubble": False,
-        "heating": False,
-        "length": 1,
-        "vertical": 1,
-        "average_slope": 1.0,
+        LiftTable.lift_id: "w1001",
+        LiftTable.mountain_id: 1,
+        LiftTable.geometry: LineString([[1, 1], [0, 0]]),
+        LiftTable.name: "Test",
+        LiftTable.lift_type: "chair_lift",
+        LiftTable.occupancy: 4,
+        LiftTable.capacity: 1200,
+        LiftTable.detachable: False,
+        LiftTable.bubble: False,
+        LiftTable.heating: False,
+        LiftTable.length: 1,
+        LiftTable.vertical: 1,
+        LiftTable.average_slope: 1.0,
     }
 
     return Lift(**lift_dict)
