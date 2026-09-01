ba705b7f1794d3e87e32f378bbf1c4a8f091308f
joshua-coppola <joshua.coppola99@gmail.com>
2026-09-01 10:48:35 -0400

    cache weather table

--- config/cache_db.sql
+++ config/cache_db.sql
@@ -1,7 +1,16 @@
-DROP TABLE IF EXISTS CachedPoints;
-
-CREATE TABLE "CachedPoints"
+CREATE TABLE IF NOT EXISTS "CachedPoints"
 (
     "point" TEXT PRIMARY KEY NOT NULL,
     "elevation" REAL NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "CachedWeather"
+(
+    "point" TEXT NOT NULL,
+    "season" INTEGER NOT NULL,  -- calendar year the winter starts (November)
+    "month" INTEGER NOT NULL,   -- calendar month: 11, 12, 1, 2, 3, 4
+    "icy_days" INTEGER NOT NULL,
+    "rain" REAL NOT NULL,
+    "snow" REAL NOT NULL,
+    PRIMARY KEY ("point", "season", "month")
 );
